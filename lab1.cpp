ce605baac001751321a25c717dd9a45694d68b2c
alpa4 <125640473+alpa4@users.noreply.github.com>
2025-07-06 17:46:20 +0300

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,267 @@
+﻿#include <iostream>
+#include <vector>
+#include <chrono>
+#include <fstream>
+#include <omp.h>
+#include <stdexcept>
+
+void partial_sum_parallel(std::vector<size_t>& arr, size_t num_threads) 
+{
+    size_t n = arr.size();
+    if (n <= 1) return;
+
+    
+    size_t chunk_size = 0;
+    size_t total_threads = std::min(num_threads, n/2);
+    chunk_size = (n + total_threads - 1) / total_threads; 
+    total_threads = (n + chunk_size - 1) / chunk_size; 
+    
+
+
+
+    
+#pragma omp parallel num_threads(total_threads)
+    {
+        size_t thread_id = omp_get_thread_num();
+        
+
+        size_t start = thread_id * chunk_size; 
+        size_t end = std::min(start + chunk_size, n); 
+
+        for (size_t i = start + 1; i < end; ++i) {
+            arr[i] += arr[i - 1];
+        }
+    
+
+#pragma omp barrier
+#pragma omp single
+        {
+            
+            for (size_t block = 1; block < total_threads; ++block) {
+                size_t prev_last = block * chunk_size - 1; 
+                size_t curr_last = std::min((block + 1) * chunk_size - 1, n - 1);   
+                arr[curr_last] += arr[prev_last];
+            }
+        }
+
+
+
+        
+        if (thread_id != 0)
+        {
+            size_t prev_last = thread_id * chunk_size - 1; 
+            size_t start = thread_id * chunk_size;        
+            size_t end = std::min(start + chunk_size - 1, n - 1); 
+
+            for (size_t i = start; i < end; ++i) {
+                arr[i] += arr[prev_last];
+            }
+        }
+    }
+}
+
+void partial_sum_parallel(std::vector<size_t>& arr) {
+    size_t n = arr.size();
+    if (n <= 1) return;
+
+    size_t total_threads = 0;
+    size_t chunk_size = 0;
+
+
+
+    
+#pragma omp parallel
+    {
+#pragma omp single
+        {
+            
+            total_threads = omp_get_num_threads();
+            total_threads = std::min(n/2, total_threads);
+            chunk_size = (n + total_threads - 1) / total_threads; 
+            total_threads = (n + chunk_size - 1) / chunk_size; 
+        }
+        size_t thread_id = omp_get_thread_num();
+        size_t start = thread_id * chunk_size; 
+        size_t end = std::min(start + chunk_size, n); 
+
+        for (size_t i = start + 1; i < end; ++i) {
+            arr[i] += arr[i - 1];
+        }
+
+    
+
+#pragma omp barrier
+#pragma omp single
+        {
+            
+            for (size_t block = 1; block < total_threads; ++block) {
+                size_t prev_last = block * chunk_size - 1; 
+                size_t curr_last = std::min((block + 1) * chunk_size - 1, n - 1);   
+                arr[curr_last] += arr[prev_last];
+            }   
+        }
+
+
+
+    
+        if (thread_id != 0)
+        {
+            size_t prev_last = thread_id * chunk_size - 1; 
+            size_t start = thread_id * chunk_size;        
+            size_t end = std::min(start + chunk_size - 1, n - 1); 
+
+            for (size_t i = start; i < end; ++i) {
+                arr[i] += arr[prev_last];
+            }
+        }
+    }
+}
+
+void partial_summ_seq(std::vector<size_t>& arr)
+{
+    size_t n = arr.size();
+    if (n <= 1)
+        return;
+    for (size_t i = 1; i < n; i++)
+    {
+        arr[i] += arr[i - 1];
+    }
+}
+
+
+
+size_t find_threshold() {
+    size_t M = std::pow(2, 25);
+    
+    double seq_time, par_time;
+    std::vector<size_t> arr;
+    std::vector<size_t> arr1;
+
+
+    while (true) {
+        arr.assign(M, 1);
+        auto start = std::chrono::high_resolution_clock::now();
+        
+        partial_summ_seq(arr);
+        auto end = std::chrono::high_resolution_clock::now();
+        seq_time = std::chrono::duration<double>(end - start).count();
+
+        arr1.assign(M, 1);
+        start = std::chrono::high_resolution_clock::now();
+        
+        partial_sum_parallel(arr1);
+        end = std::chrono::high_resolution_clock::now();
+        par_time = std::chrono::duration<double>(end - start).count();
+
+        bool flag=false;
+        for (int i = 0; i < M; i++)
+        {
+            
+            if (arr[i] != arr1[i])
+            {
+                flag=true;
+                break;
+            }
+        }
+        if (flag)
+        {
+            for (auto ar : arr1)
+                printf("%d", ar);
+            break;
+        }
+
+
+        std::cout << "Parallel algorithm time " << par_time << " , sequence " << seq_time << " , M = " << M << std::endl; 
+
+        if (par_time < seq_time) break;
+        M *= 2;
+    }
+
+    
+    std::ofstream file("threshold.txt");
+    file << M;
+    file.close();
+
+    return M;
+}
+
+
+void partial_sum_hybrid(std::vector<size_t>& arr) {
+    size_t n = arr.size();
+    if (n <= 1) return;
+
+    
+    size_t M;
+    std::ifstream file("threshold.txt");
+    if (file.is_open()) {
+        file >> M;
+        file.close();
+    }
+    else {
+        throw std::runtime_error("Не удалось открыть файл threshold.txt");
+    }
+
+    if (n >= M) {
+        
+        partial_sum_parallel(arr);
+    }
+    else {
+        
+        partial_summ_seq(arr);
+    }
+}
+
+
+void benchmark(size_t N) {
+    std::vector<size_t> arr(N, 1);
+    std::ofstream file("speedup.csv");
+    file << "Threads,Time\n";
+
+    for (size_t threads = 1; threads <= omp_get_max_threads(); ++threads) {
+        auto start = std::chrono::high_resolution_clock::now();
+        partial_sum_parallel(arr, threads);
+        auto end = std::chrono::high_resolution_clock::now();
+
+        double elapsed = std::chrono::duration<double>(end - start).count();
+        file << threads << "," << elapsed << "\n";
+        std::cout << threads << "," << elapsed << "\n";
+    }
+
+    file.close();
+}
+
+int main() {
+    try {
+        
+        std::cout << "Counting threshold M...\n";
+        size_t M = find_threshold();
+        std::cout << "Threshold meaning M: " << M << "\n";
+        
+        
+        std::cout << "Hybrid algorithm...\n";
+        size_t N = 2 * M;
+        std::vector<size_t> arr(N, 1);
+
+        partial_sum_hybrid(arr);
+
+        
+        std::cout << "Partial summ counting results (first 10):\n";
+        for (size_t i = 0; i < std::min(size_t(10), N); ++i) {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << "\n";
+        
+        
+        std::cout << "Time counting for speedup diagram...\n";
+        benchmark(N);
+        std::cout << "Results saved in speedup.csv.\n";
+        
+    }
+    catch (const std::exception& ex) {
+        std::cerr << "Error: " << ex.what() << "\n";
+        return 1;
+    }
+
+
+    return 0;
+}
---
+++ lab2.cpp
@@ -0,0 +1,151 @@
+﻿#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+
+#define TERMINATE_MSG -1
+#define FREE_PROCESS_MSG -2
+
+// Простейшие функции конвейера
+float f1(float x) { return x + 1.0; }
+float f2(float x) { return x * 2.0; }
+float f3(float x) { return x - 3.0; }
+
+// Функции для удобного изменения порядка вычислений
+typedef float (*func_ptr)(float);
+std::vector<func_ptr> functions = { f1, f2, f3 };
+
+using namespace std;
+
+void process_zero(int data_size, float data[], int broker_rank) {
+    for (int i = 0; i < data_size; i++) {
+        float msg[2] = { static_cast<float>(i), data[i] };
+        MPI_Send(msg, 2, MPI_FLOAT, broker_rank, 0, MPI_COMM_WORLD);
+        cout << "0 процесс отправил переменную " << msg[0] << " со значением " << msg[1] << endl;
+    }
+}
+
+void process_worker(int stage, int broker_in, int broker_out) {
+    while (1) {
+        float msg[2];
+        MPI_Recv(&msg, 2, MPI_FLOAT, broker_in, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if (static_cast<int>(msg[0]) == TERMINATE_MSG) break;
+
+        cout << "переменная " << msg[0] << " со значением " << msg[1] <<  "стадия" << endl;
+
+        msg[1] = functions[stage](msg[1]);
+        MPI_Send(msg, 2, MPI_FLOAT, broker_out, 0, MPI_COMM_WORLD);
+
+        float free_msg[2] = { static_cast<float>(FREE_PROCESS_MSG), static_cast<float>(broker_in) };
+        MPI_Send(free_msg, 2, MPI_FLOAT, broker_in, 0, MPI_COMM_WORLD);
+    }
+}
+
+void process_broker(std::vector<int> workers, int data_size) {
+    std::vector<std::pair<int, float>> queue;
+    std::vector<int> free_workers = workers;
+    int received_count = 0;
+
+    MPI_Status st;
+
+    // Получаем все данные
+    while (received_count < data_size || !queue.empty() || free_workers.size() < workers.size()) {
+        float msg[2];
+        MPI_Recv(&msg, 2, MPI_FLOAT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &st);
+
+        if (static_cast<int>(msg[0]) == FREE_PROCESS_MSG) {
+            free_workers.push_back(st.MPI_SOURCE);
+        }
+        else {
+            queue.push_back({ static_cast<int>(msg[0]), msg[1] });
+            received_count++;
+        }
+
+        // Распределяем задачи среди свободных рабочих
+        while (!queue.empty() && !free_workers.empty()) {
+            int worker = free_workers.back();
+            free_workers.pop_back();
+
+            auto task = queue.front();
+            queue.erase(queue.begin());
+            float send_msg[2] = { static_cast<float>(task.first), task.second };
+            MPI_Send(send_msg, 2, MPI_FLOAT, worker, 0, MPI_COMM_WORLD);
+        }
+    }
+
+    // Дожидаемся освобождения всех рабочих
+    while (free_workers.size() < workers.size()) {
+        float msg[2];
+        MPI_Recv(&msg, 2, MPI_FLOAT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &st);
+
+        if (static_cast<int>(msg[0]) == FREE_PROCESS_MSG) {
+            free_workers.push_back(st.MPI_SOURCE);
+        }
+    }
+
+    // Отправляем всем рабочим процессам сообщение о завершении работы
+    for (int w : workers) {
+        float term_msg[2] = { static_cast<float>(TERMINATE_MSG), static_cast<float>(TERMINATE_MSG) };
+        MPI_Send(term_msg, 2, MPI_FLOAT, w, 0, MPI_COMM_WORLD);
+    }
+}
+
+void process_sum(int total_count) {
+    std::vector<float> results(total_count);
+    float sum = 0.0;
+    for (int i = 0; i < total_count; i++) {
+        float msg[2];
+        MPI_Recv(&msg, 2, MPI_FLOAT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        results[static_cast<int>(msg[0])] = msg[1];
+        sum += msg[1];
+    }
+    for (int i = 0; i < total_count; i++) {
+        printf("Result[%d] = %.2f\n", i, results[i]);
+    }
+    printf("Total Sum: %.2f\n", sum);
+}
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int data_size = 5;
+    float data[] = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+    int distribution[] = { 1, 2, 3 };
+    int num_stages = functions.size();
+
+    if (rank == 0) {
+        process_zero(data_size, data, 1);
+    }
+    else if (rank == size - 1) {
+        process_sum(data_size);
+    }
+    else {
+        int broker_rank = 1;
+        int worker_start = 2;
+
+        for (int stage = 0; stage < num_stages; stage++) {
+            int num_workers = distribution[stage];
+            std::vector<int> workers;
+            for (int i = 0; i < num_workers; i++) {
+                workers.push_back(worker_start + i);
+            }
+
+            if (rank == broker_rank) {
+                process_broker(workers, data_size);
+            }
+            else if (std::find(workers.begin(), workers.end(), rank) != workers.end()) {
+                process_worker(stage, broker_rank, workers.back()+1);
+            }
+
+            broker_rank += num_workers + 1;
+            worker_start += num_workers;
+        }
+    }
+
+    MPI_Finalize();
+    return 0;
+}
